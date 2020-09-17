ebc1c9eb932882b31c2b7a25872bc42209ba2ef1
Dell_Blue <2470334784@qq.com>
2020-09-17 11:02:30 +0800

    200917

--- Read_contact/app/src/main/java/com/example/read_contact/MainActivity.java
+++ Read_contact/app/src/main/java/com/example/read_contact/MainActivity.java
@@ -16,21 +16,26 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView = null;
+    private ArrayAdapter<String> adapter;
+    private String[] datas = {"暂未读取联系人"};
+    private ListView listView;
     private final String tag = "mytag";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        final ArrayList<String> items = new ArrayList<>();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.btn_getmsg);
+
+        listView = (ListView)findViewById(R.id.listview_list);
+        adapter = new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,datas);
+        listView.setAdapter(adapter);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.v(tag,"按钮点击");
+                ArrayList<String> items = new ArrayList<>();
                 Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
                 while (cursor.moveToNext()){
 
@@ -49,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.v(tag,items.get(0).toString());
                 String [] array_items = new String[items.size()];
                 items.toArray(array_items);
-                listView = listView.findViewById(R.id.listview_list);
-                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,R.layout.list_view,array_items);
+                //刷新Adapter
+                adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_expandable_list_item_1,array_items);
                 listView.setAdapter(adapter);
             }
         });
