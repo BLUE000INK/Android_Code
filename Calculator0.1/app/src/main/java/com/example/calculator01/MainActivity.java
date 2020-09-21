5eb0a2866664505c00da2719a9532f159072a285
Dell_Blue <2470334784@qq.com>
2020-09-21 08:51:24 +0800

    200921

--- Calculator0.1/app/src/main/java/com/example/calculator01/MainActivity.java
+++ Calculator0.1/app/src/main/java/com/example/calculator01/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.calculator01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,179 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText max_text = findViewById(R.id.edittext_max);
+        final TextView min_text = findViewById(R.id.textview_min);
+
+        Button btn_num_0 = findViewById(R.id.btn_num_0);
+        Button btn_num_1 = findViewById(R.id.btn_num_1);
+        Button btn_num_2 = findViewById(R.id.btn_num_2);
+        Button btn_num_3 = findViewById(R.id.btn_num_3);
+        Button btn_num_4 = findViewById(R.id.btn_num_4);
+        Button btn_num_5 = findViewById(R.id.btn_num_5);
+        Button btn_num_6 = findViewById(R.id.btn_num_6);
+        Button btn_num_7 = findViewById(R.id.btn_num_7);
+        Button btn_num_8 = findViewById(R.id.btn_num_8);
+        Button btn_num_9 = findViewById(R.id.btn_num_9);
+
+        Button btn_op_add = findViewById(R.id.btn_op_add);
+        Button btn_op_jian = findViewById(R.id.btn_op_jian);
+        Button btn_op_x = findViewById(R.id.btn_op_x);
+        Button btn_op_chu = findViewById(R.id.btn_op_chu);
+
+        Button btn_op_left_k = findViewById(R.id.btn_op_left_k);
+        Button btn_op_righ_k = findViewById(R.id.btn_op_right_k);
+        Button btn_num_point = findViewById(R.id.btn_num_point);
+
+        Button btn_op_result = findViewById(R.id.btn_op_result);
+        Button btn_op_CE = findViewById(R.id.btn_op_CE);
+
+        btn_num_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"0");
+            }
+        });
+
+        btn_num_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"1");
+            }
+        });
+
+        btn_num_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"2");
+            }
+        });
+
+        btn_num_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"3");
+            }
+        });
+
+        btn_num_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"4");
+            }
+        });
+
+        btn_num_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"5");
+            }
+        });
+
+        btn_num_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"6");
+            }
+        });
+
+        btn_num_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"7");
+            }
+        });
+
+        btn_num_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"8");
+            }
+        });
+
+        btn_num_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+"9");
+            }
+        });
+
+        btn_num_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                max_text.setText(max_text.getText()+".");
+            }
+        });
+        btn_op_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+"+");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_jian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+"-");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+"X");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_chu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+"/");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_left_k.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+"(");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_righ_k.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+")");
+                max_text.setText("");
+            }
+        });
+
+        btn_op_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText(min_text.getText().toString()+max_text.getText().toString()+"=");
+                Calculator_base str = new Calculator_base();
+                try {
+                    float result = str.caculate(min_text.getText().toString());
+                    max_text.setText(Float.toString(result));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this,"算式错误！",Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        btn_op_CE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                min_text.setText("");
+                max_text.setText("");
+            }
+        });
     }
 }
