27512b6892db85761593b95c241d371a1b11fcea
Dell_Blue <2470334784@qq.com>
2020-10-04 15:39:46 +0800

    201004

--- Calculator_service/app/src/main/java/com/example/calculator_service/MainActivity.java
+++ Calculator_service/app/src/main/java/com/example/calculator_service/MainActivity.java
@@ -2,21 +2,42 @@ package com.example.calculator_service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.security.Provider;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyService myService = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ServiceConnection serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                Log.v("mytag","onServiceConnected");
+                myService=((MyService.LocalBinder)iBinder).getService();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                Log.v("mytag","onServiceDisconnected");
+            }
+        };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn_cal = findViewById(R.id.btn_cal);
         Button btn_stop = findViewById(R.id.btn_stop);
+        final TextView textView = findViewById(R.id.text_result);
         final EditText editText = findViewById(R.id.edittext_str);
 
         btn_cal.setOnClickListener(new View.OnClickListener() {
@@ -24,7 +45,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,MyService.class);
                 intent.putExtra("str",editText.getText().toString());
-                startService(intent);
+                Log.v("mytag","str="+editText.getText().toString());
+                //startService(intent);
+                bindService(intent,serviceConnection, Service.BIND_AUTO_CREATE);
+                Log.v("mytag","222");
+                textView.setText(myService.calculate(editText.getText().toString()));
             }
         });
 
--- Calculator_service/app/src/main/java/com/example/calculator_service/MyService.java
+++ Calculator_service/app/src/main/java/com/example/calculator_service/MyService.java
@@ -2,22 +2,25 @@ package com.example.calculator_service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyService extends Service {
+    private LocalBinder myBinder = new LocalBinder();
     public MyService() {
     }
 
     @Override
     public void onCreate() {
-        Toast.makeText(this,"计算服务开始运行",Toast.LENGTH_LONG);
+        Log.v("mytag","onCreat");
         super.onCreate();
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this,"计算服务停止运行",Toast.LENGTH_LONG);
+        Log.v("mytag","onDestroy");
         super.onDestroy();
     }
 
@@ -27,17 +30,38 @@ public class MyService extends Service {
         try{
             String str = intent.getStringExtra("str");
             Calculator_base calculator_base = new Calculator_base();
-            Toast.makeText(this,"计算结果为："+calculator_base.caculate(str),Toast.LENGTH_LONG);
+
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(this,"算式错误！",Toast.LENGTH_LONG);
         }
         return super.onStartCommand(intent, flags, startId);
     }
 
+    public class LocalBinder extends Binder {
+        MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    public String calculate(String str){
+        Log.v("mytag","calculate--"+str);
+        Calculator_base calculator = new Calculator_base();
+        try{
+           return calculator.caculate(str)+"";
+        } catch (Exception e) {
+            return "算式错误,计算失败";
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.v("mytag","onBinder()");
+        return myBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v("mytag","onUnbind()");
+        return super.onUnbind(intent);
     }
 }
