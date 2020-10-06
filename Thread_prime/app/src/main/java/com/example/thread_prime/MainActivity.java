f64c9e0769943ee27a38494bc1217834838f5b60
Dell_Blue <2470334784@qq.com>
2020-10-06 17:16:27 +0800

    201006

--- Thread_prime/app/src/main/java/com/example/thread_prime/MainActivity.java
+++ Thread_prime/app/src/main/java/com/example/thread_prime/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +16,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Runnable myWOrker = new Runnable() {
+        final EditText editText = findViewById(R.id.edittext_in);
+        final TextView textView = findViewById(R.id.text_out);
+
+        final Runnable myWorker = new Runnable() {
             @Override
             public void run() {
-                long endTime = System.currentTimeMillis() + 10 * 1000;
-
-                while(System.currentTimeMillis()<endTime)
+                Log.v("mytag","线程运行");
                     while(!Thread.interrupted()){
                         synchronized (this){
-                            try{
-                                Log.v("mytag","运算中");
-                                wait();
-                            } catch (InterruptedException e) {
+                            try {
+                                int num = Integer.parseInt(editText.getText().toString());
+                                int result = 1;
+                                String str;
+                                for (int i = 2; i < num && result == 1; i++) {
+                                    if (num % i == 0) {
+                                        result = 0;
+                                    }
+                                }
+                                Log.v("mytag", "result=" + result);
+                                if(result == 0)
+                                    str = "非素数";
+                                else
+                                    str = "素数";
+                                textView.setText(str);
+                                Thread.interrupted();
+                            } catch (Exception e){
                                 e.printStackTrace();
                             }
                         }
                     }
+                Log.v("mytag","运算结束");
             }
         };
+        Button button = findViewById(R.id.btn_judge);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null,myWorker,"WorkThread");
+                workThread.start();
+            }
+        });
     }
 }
